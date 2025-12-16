9d0a8c7faf9176e322d68ba15d24fbc32109c1ea
Ericson <eebaluyotbpi@gmail.com>
2025-12-16 13:38:27 +0800

    M2 Activity 2

---
+++ M2_Activity2/SeatWork2.java
@@ -0,0 +1,12 @@
+package m2.actvity.two;
+
+
+
+public class SeatWork2 {
+	public static void main(String[] args) {
+		
+		Whiteboard wb = new Whiteboard();
+		int results = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(results) );
+	}
+}
---
+++ M2_Activity2/Whiteboard.java
@@ -0,0 +1,11 @@
+package m2.actvity.two;
+
+
+
+public class Whiteboard {
+		
+	int addNumbers(int addend1, int addend2){
+		int results = addend1 + addend2;
+		return results;
+	}
+}
