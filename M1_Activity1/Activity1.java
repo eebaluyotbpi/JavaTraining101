81fb75ec6751e6125f0636ed9ef1201e87638634
Ericson <eebaluyotbpi@gmail.com>
2025-12-16 12:39:45 +0800

    M1 Activity 1

---
+++ M1_Activity1/Activity1.java
@@ -0,0 +1,23 @@
+package m1.actvity.one;
+import java.util.Scanner;
+
+
+public class Activity1 {
+	
+	 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Welcome to BPI Java Training!");
+		System.out.println("What is your name?");
+		String name = sc.next();
+		
+		System.out.printf("Your name is %s", name);
+		
+		
+	}
+
+}
