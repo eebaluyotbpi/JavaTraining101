bf9092897a1fa50ff8663bd479e048a4e7191d27
eebaluyot <eebaluyotbpi@gmail.com>
2026-03-11 16:00:25 +0800

    Module 9 Exercise #1

---
+++ src/main/java/com/bpi/training/Module9Exe1/Module9Exe1Application.java
@@ -0,0 +1,13 @@
+package com.bpi.training.Module9Exe1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module9Exe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module9Exe1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpi/training/Module9Exe1/Module9Exe1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpi.training.Module9Exe1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module9Exe1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
